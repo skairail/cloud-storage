c435e01daa86cbb4213f48ba3fab951905578f0e
Oleksandr <skairail1600@gmail.com>
2024-01-07 21:08:52 +0200

    chore(client): code refactor and styles updating

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styles from "./App.module.css";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Registration from "./components/Authorization/Registration";
 import Login from "./components/Authorization/Login";
 import { useDispatch, useSelector } from "react-redux";
 import { auth } from "../src/actions/user";
+import Disk from "./components/Disk/Disk";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   const isAuth = useSelector((state: any) => state.user.isAuth);
@@ -16,15 +19,23 @@ function App() {
 
   return (
     <BrowserRouter>
-      <div className="app">
+      <div className={styles.app}>
         <Navbar />
-        <div className="wrap">
-          {!isAuth && (
-            <Routes>
-              <Route path="/registration" element={<Registration />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-          )}
+        <div className={styles.wrap}>
+          <Routes>
+            {!isAuth ? (
+              <>
+                <Route path="/registration" element={<Registration />} />
+                <Route path="/login" element={<Login />} />
+              </>
+            ) : (
+              <>
+                <Route path="/" element={<Disk />} />
+                <Route path="/profile" element={<Profile />} />
+              </>
+            )}
+          </Routes>
+          {isAuth && <Navigate to="/" />}
         </div>
       </div>
     </BrowserRouter>
